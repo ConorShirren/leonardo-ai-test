0f13da21f59ef5ea3264ac5b466b3ce48136dd54
Conor Shirren <cshirren@gmail.com>
2024-04-26 07:15:08 +1000

    test: add unit tests to routes

--- src/routes/__tests__/scheduleRoutes.test.ts
+++ src/routes/__tests__/scheduleRoutes.test.ts
@@ -1,7 +1,13 @@
+import {
+  createSchedule,
+  deleteSchedule,
+  fetchSchedules,
+  getSchedule,
+  updateSchedule,
+} from '../../controller/ScheduleController';
 import express, { Request, Response } from 'express';
 
 import { Schedule } from '../../types/schedule';
-import { fetchSchedules } from '../../controller/ScheduleController';
 import request from 'supertest';
 import router from '../../routes/scheduleRoutes';
 
@@ -11,9 +17,13 @@ app.use('/', router);
 
 jest.mock('../../controller/ScheduleController', () => ({
   fetchSchedules: jest.fn(),
+  getSchedule: jest.fn(),
+  createSchedule: jest.fn(),
+  deleteSchedule: jest.fn(),
+  updateSchedule: jest.fn(),
 }));
 
-describe('fetchTasks', () => {
+describe('fetchSchedules', () => {
   const mockedDate = new Date('2024-04-25T12:00:00Z');
 
   beforeEach(() => {
@@ -26,14 +36,14 @@ describe('fetchTasks', () => {
     jest.restoreAllMocks();
   });
 
-  it('should call fetchTasks controller function and return 200 status code with tasks', async () => {
+  it('should call fetchSchedules controller function and return 200 status code with schedules', async () => {
     const schedules: Schedule[] = [
       {
         id: '123e4567-e89b-12d3-a456-426614174000',
         account_id: 1,
-        agent_id: 12345,
-        start_time: mockedDate,
-        end_time: mockedDate,
+        // start_time: mockedDate,
+        // end_time: mockedDate,
+        agent_id: 1234,
         tasks: [],
       },
     ];
@@ -48,5 +58,107 @@ describe('fetchTasks', () => {
     expect(response.status).toBe(200);
     expect(fetchSchedules).toHaveBeenCalledTimes(1);
     expect(response.body).toEqual({ status: 200, data: schedules });
+  });
+});
+
+describe('getSchedule', () => {
+  it('should call getSchedule controller function and return 200 status code with schedule', async () => {
+    const schedule: Schedule = {
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      account_id: 1,
+      // start_time: mockedDate,
+      // end_time: mockedDate,
+      agent_id: 1234,
+      tasks: [],
+    };
+
+    (getSchedule as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, data: schedule });
+      }
+    );
+
+    const response = await request(app).get(
+      '/id=123e4567-e89b-12d3-a456-426614174000'
+    );
+    expect(response.status).toBe(200);
+    expect(getSchedule).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({ status: 200, data: schedule });
+  });
+});
+
+describe('createSchedule', () => {
+  it('should call getSchedule controller function and return 200 status code with schedule', async () => {
+    const schedule: Schedule = {
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      account_id: 1,
+      // start_time: mockedDate,
+      // end_time: mockedDate,
+      agent_id: 1234,
+      tasks: [],
+    };
+
+    (createSchedule as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, data: schedule, msg: 'Schedule created.' });
+      }
+    );
+
+    const response = await request(app).post('/').send(schedule);
+    expect(response.status).toBe(200);
+    expect(createSchedule).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      data: schedule,
+      msg: 'Schedule created.',
+    });
+  });
+});
+describe('updateSchedule', () => {
+  it('should call getSchedule controller function and return 200 status code with schedule', async () => {
+    const schedule: Schedule = {
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      account_id: 1,
+      // start_time: mockedDate,
+      // end_time: mockedDate,
+      agent_id: 1234,
+      tasks: [],
+    };
+
+    (updateSchedule as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, message: 'Schedule updated successfully' });
+      }
+    );
+
+    const response = await request(app)
+      .put('/id=123e4567-e89b-12d3-a456-426614174000')
+      .send(schedule);
+    expect(response.status).toBe(200);
+    expect(updateSchedule).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      message: 'Schedule updated successfully',
+    });
+  });
+});
+
+describe('deleteSchedule', () => {
+  it('should call getSchedule controller function and return 200 status code with schedule', async () => {
+    (deleteSchedule as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, msg: 'Schedule deleted successfully' });
+      }
+    );
+
+    const response = await request(app).delete(
+      '/id=123e4567-e89b-12d3-a456-426614174000'
+    );
+    expect(response.status).toBe(200);
+    expect(deleteSchedule).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      msg: 'Schedule deleted successfully',
+    });
   });
 });
--- src/routes/__tests__/taskRoutes.test.ts
+++ src/routes/__tests__/taskRoutes.test.ts
@@ -1,7 +1,13 @@
 import { Task, TaskType } from '../../types/task';
+import {
+  createTask,
+  deleteTask,
+  fetchTasks,
+  getTask,
+  updateTask,
+} from '../../controller/TaskController';
 import express, { Request, Response } from 'express';
 
-import { fetchTasks } from '../../controller/TaskController';
 import request from 'supertest';
 import router from '../../routes/taskRoutes';
 
@@ -11,6 +17,10 @@ app.use('/', router);
 
 jest.mock('../../controller/TaskController', () => ({
   fetchTasks: jest.fn(),
+  getTask: jest.fn(),
+  createTask: jest.fn(),
+  deleteTask: jest.fn(),
+  updateTask: jest.fn(),
 }));
 
 describe('fetchTasks', () => {
@@ -61,7 +71,7 @@ describe('getTask', () => {
       type: TaskType.WORK,
     };
 
-    (fetchTasks as jest.Mock).mockImplementationOnce(
+    (getTask as jest.Mock).mockImplementationOnce(
       (req: Request, res: Response) => {
         res.json({ status: 200, data: task });
       }
@@ -71,7 +81,81 @@ describe('getTask', () => {
       '/id=123e4567-e89b-12d3-a456-426614174000'
     );
     expect(response.status).toBe(200);
-    expect(fetchTasks).toHaveBeenCalledTimes(1);
+    expect(getTask).toHaveBeenCalledTimes(1);
     expect(response.body).toEqual({ status: 200, data: task });
+  });
+});
+
+describe('createTask', () => {
+  it('should call getTask controller function and return 200 status code with task', async () => {
+    const task: Task = {
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      account_id: 1,
+      schedule_id: 'abc123',
+      duration: 60,
+      type: TaskType.WORK,
+    };
+
+    (createTask as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, data: task, msg: 'Task created.' });
+      }
+    );
+
+    const response = await request(app).post('/').send(task);
+    expect(response.status).toBe(200);
+    expect(createTask).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      data: task,
+      msg: 'Task created.',
+    });
+  });
+});
+describe('updateTask', () => {
+  it('should call getTask controller function and return 200 status code with task', async () => {
+    const task: Task = {
+      id: '123e4567-e89b-12d3-a456-426614174000',
+      account_id: 1,
+      schedule_id: 'abc123',
+      duration: 60,
+      type: TaskType.WORK,
+    };
+
+    (updateTask as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, message: 'Task updated successfully' });
+      }
+    );
+
+    const response = await request(app)
+      .put('/id=123e4567-e89b-12d3-a456-426614174000')
+      .send(task);
+    expect(response.status).toBe(200);
+    expect(updateTask).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      message: 'Task updated successfully',
+    });
+  });
+});
+
+describe('deleteTask', () => {
+  it('should call getTask controller function and return 200 status code with task', async () => {
+    (deleteTask as jest.Mock).mockImplementationOnce(
+      (req: Request, res: Response) => {
+        res.json({ status: 200, msg: 'Task deleted successfully' });
+      }
+    );
+
+    const response = await request(app).delete(
+      '/id=123e4567-e89b-12d3-a456-426614174000'
+    );
+    expect(response.status).toBe(200);
+    expect(deleteTask).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({
+      status: 200,
+      msg: 'Task deleted successfully',
+    });
   });
 });
